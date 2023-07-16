a579637bc14107e69643eff0253129515ed03246
FragmentaryMind <jllobejko@gmail.com>
2023-07-16 12:58:27 -0500

    Create full working version of RPS with ui
    Add image files for rock, paper, and scissors
    Implemented click event listener for RPS container. Wrote handler
    function to play a round until five combined computer/player wins.
    If computer wins by three or more, player gets destroyed and is
    punished with a color-cycling taunt.

--- javascript.js
+++ javascript.js
@@ -1,64 +1,58 @@
 // git@github.com:FragmentaryMind/Rock-Paper-Scissors.git
 
+let playerWinCount = 0;
+let computerWinCount = 0;
+let gameOver = false;
+const rpsContainer = document.querySelector(".rps-container");
+
 function notifyPlayer (message) {
-    console.log(message);
-    alert(message);
-}
-
-function getPlayerChoice () {
-    let choice = null;
-
-    while (!choice) {
-        choice = prompt("Would you like Rock, Paper, or Scissors?") ?? "";
-
-        switch (choice.toUpperCase()) {
-            case "ROCK":
-            case "PAPER":
-            case "SCISSORS":
-                return choice;
-            default:
-                choice = null;
-        }
-
-        alert("Invalid choice.");
-    }
-
-    return "Wet Fart";
-}
-
-function getComputerChoice () {
-    return ["Rock","Paper","Scissors"][Math.floor(Math.random() * 3)];
+    const outcomePara = document.querySelector(".outcome");
+    outcomePara.textContent = message;
+    const scorePara = document.querySelector(".score");
+    scorePara.textContent = `Player Victories: ${playerWinCount} Computer Victories: ${computerWinCount}.`;
 }
 
 function playRound (playerSelection, computerSelection) {
-    const ps = playerSelection.toUpperCase();
-    const cs = computerSelection.toUpperCase();
+    const ps = playerSelection;
+    const cs = computerSelection;
 
     if (ps == cs) {
         notifyPlayer(`It's a tie! You and the computer both chose ${playerSelection}.`);
         return;
     }
     
-    let playerWins = (ps == "ROCK" && cs != "PAPER") || (ps == "PAPER" && cs != "SCISSORS") || (ps == "SCISSORS" && cs != "ROCK");
+    let playerWins = (ps == "rock" && cs != "paper") || (ps == "paper" && cs != "scissors") || (ps == "scissors" && cs != "rock");
     if (playerWins) {
         playerWinCount += 1;
-        notifyPlayer(`You won! Your ${playerSelection} beats the computer's ${computerSelection}.`);
+        notifyPlayer(`Yes! Your ${playerSelection} beats the computer's ${computerSelection}.`);
     } else {
         computerWinCount += 1;
-        notifyPlayer(`The computer won! The computer's ${computerSelection} beats your ${playerSelection}.`);
+        notifyPlayer(`No! The computer's ${computerSelection} beats your ${playerSelection}.`);
+    }
+
+    if (playerWinCount + computerWinCount >= 5) gameOver = true;
+}
+
+function rpsPlayerClick (event) {
+    if (gameOver) return;
+
+    let playerChoice = event.target.getAttribute("class").replace("rps-img ","");
+    if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") return;
+    let computerChoice = ["rock","paper","scissors"][Math.floor(Math.random() * 3)];
+    playRound(playerChoice, computerChoice);
+
+    if (gameOver) {
+        const h1 = document.querySelector("h1");
+        h1.textContent = "GAME OVER";
+        const finalOutcome = document.querySelector(".final-outcome");
+        finalOutcome.textContent = playerWinCount > computerWinCount ? "You won!" : 
+                                   playerWinCount < computerWinCount ? "You lost!" : 
+                                   "It's a tie!";
+        if (computerWinCount >= playerWinCount + 3) {
+            finalOutcome.textContent = "*** YOU GOT DESTROYED ***";
+            finalOutcome.setAttribute("class","awesome");
+        }
     }
 }
 
-function game() {
-    playRound(getPlayerChoice(), getComputerChoice());
-    playRound(getPlayerChoice(), getComputerChoice());
-    playRound(getPlayerChoice(), getComputerChoice());
-    playRound(getPlayerChoice(), getComputerChoice());
-    playRound(getPlayerChoice(), getComputerChoice());
-}
-
-let playerWinCount = 0;
-let computerWinCount = 0;
-game();
-let outcomeNotification = playerWinCount > computerWinCount ? "You won!" : playerWinCount < computerWinCount ? "You lost!" : "It's a tie!";
-notifyPlayer(`${outcomeNotification} Player Victories: ${playerWinCount} Computer Victories: ${computerWinCount}.`);
+rpsContainer.addEventListener('click', rpsPlayerClick);
